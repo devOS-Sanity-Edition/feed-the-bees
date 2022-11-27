abbbb14ca39ad909ea388e193b7941968b526ae2
HestiMae <hestiaheartstrings@gmail.com>
2022-11-27 14:10:36 +1100

    Remove speed-up of bees
    bump to 1.2.1

--- src/main/java/garden/hestia/feed_the_bees/mixin/BeeEntityMixin.java
+++ src/main/java/garden/hestia/feed_the_bees/mixin/BeeEntityMixin.java
@@ -1,7 +1,6 @@
 package garden.hestia.feed_the_bees.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.BeeEntity;
@@ -20,16 +19,6 @@ public abstract class BeeEntityMixin extends AnimalEntity {
 
 	@Shadow
 	protected abstract void setHasStung(boolean hasStung);
-
-	@ModifyArgs(method = "createBeeAttributes",
-	at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;add(Lnet/minecraft/entity/attribute/EntityAttribute;D)Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;"))
-	private static void modifyBeeSpeed(Args args)
-	{
-		if (args.get(0) == EntityAttributes.GENERIC_MOVEMENT_SPEED || args.get(0) == EntityAttributes.GENERIC_FLYING_SPEED)
-		{
-			args.set(1, (double) args.get(1) * 2.0F);
-		}
-	}
 
 	@ModifyArgs(method = "mobTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/BeeEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
 	protected void mobTick(Args args)
