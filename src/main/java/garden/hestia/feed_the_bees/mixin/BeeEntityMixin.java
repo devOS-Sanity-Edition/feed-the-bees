39214ffff21714e500aabb311b6f8feb0bc33160
HestiMae <hestiaheartstrings@gmail.com>
2023-06-30 13:58:41 +1000

    Fix 1.20 crash

--- src/main/java/garden/hestia/feed_the_bees/mixin/BeeEntityMixin.java
+++ src/main/java/garden/hestia/feed_the_bees/mixin/BeeEntityMixin.java
@@ -1,33 +1,21 @@
 package garden.hestia.feed_the_bees.mixin;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.BeeEntity;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(BeeEntity.class)
-public abstract class BeeEntityMixin extends AnimalEntity {
-	protected BeeEntityMixin(EntityType<? extends AnimalEntity> entityType, World world) {
-		super(entityType, world);
-	}
+public abstract class BeeEntityMixin {
 
 	@Shadow
 	protected abstract void setHasStung(boolean hasStung);
 
-	@ModifyArgs(method = "mobTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/BeeEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
-	protected void mobTick(Args args)
-	{
-		if (args.get(0) == DamageSource.GENERIC)
-		{
-			args.set(1, 1.0F);
-			this.setHasStung(false);
-		}
+	@ModifyArg(method = "mobTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/BeeEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", ordinal = 1), index = 1)
+	protected float regenerateStinger(float amount) {
+		this.setHasStung(false);
+		return 1.0F;
 	}
 
 
